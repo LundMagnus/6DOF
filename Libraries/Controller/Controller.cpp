2335a123f815e0ba838f6b4da013b7685068b408
LundMagnus <lundmagnus03@gmail.com>
2026-03-03 17:48:49 +0100

    max retries

--- Libraries/Controller/Controller.cpp
+++ Libraries/Controller/Controller.cpp
@@ -24,7 +24,7 @@ namespace {
     #define RB   5
     #define MIN  6
     #define PLU  7
-    #define MAX_RETRIES 30
+    #define MAX_RETRIES 120
 
     int16_t X_LS_VALUE = 0;
     int16_t Y_LS_VALUE = 0;
