641b52c21abb4a821f074fff7d1c600a4af1a70f
LundMagnus <lundmagnus03@gmail.com>
2026-03-12 23:25:51 +0100

    debugger7

--- Libraries/PCA9685/PCA9685.cpp
+++ Libraries/PCA9685/PCA9685.cpp
@@ -332,9 +332,9 @@ bool PCA9685::setSmoothServoAngle(uint8_t channel, uint8_t servoType, uint16_t s
     
     if (std::abs(delta) >= static_cast<int>(smoothness) * 2) {
         if (delta > 0) {
-            currentAngle[channel] = static_cast<uint16_t>(currentAngle[channel] + log10(delta));
+            currentAngle[channel] = static_cast<uint16_t>(currentAngle[channel] + delta);
         } else {
-            currentAngle[channel] = static_cast<uint16_t>(currentAngle[channel] - log10(delta));
+            currentAngle[channel] = static_cast<uint16_t>(currentAngle[channel] - delta);
         }
         //std::cout << ", log10 of Delta: " << delta << ". Current angle of " << (int)channel << " is " << (int)currentAngle[channel] << std::endl;
     } else {
