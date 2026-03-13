6edf95a06be7b84a6be66604fc0eabee8b85f8df
LundMagnus <lundmagnus03@gmail.com>
2026-03-13 02:04:05 +0100

    000

--- Libraries/PCA9685/PCA9685.cpp
+++ Libraries/PCA9685/PCA9685.cpp
@@ -352,8 +352,7 @@ bool PCA9685::setSmoothServoAngle(uint8_t channel, uint8_t servoType, uint16_t s
         } else {
             currentAngle[channel] = static_cast<uint16_t>(currentAngle[channel] - speed);
         }
-        //std::cout << "Delta: " << delta << ". Current angle of " << (int)channel << " is " << (int)currentAngle[channel] << std::endl;
-    } else {
+        
         currentAngle[channel] = servoAngle;
     }
 
