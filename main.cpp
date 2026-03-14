fe00289241307469b59d48e041fb2a356a9fe526
LundMagnus <lundmagnus03@gmail.com>
2026-03-14 01:56:11 +0100

    test8

--- main.cpp
+++ main.cpp
@@ -170,7 +170,7 @@ int main() {
 
         pwm.setSmoothServoAngle(BASE, MS62_SERVO, 135, 2);
         usleep(1000);
-        pwm.setSmoothServoAngle(SHOULDER, MS62_SERVO, 180, 2);
+        pwm.setSmoothServoAngle(SHOULDER, MS62_SERVO, 220, 2);
 
 
         usleep(100000);
