800a19641b5621673127b92353c4dac3f3dfb1e5
LundMagnus <lundmagnus03@gmail.com>
2026-07-11 23:36:18 +0200

    why does .h files even exist

--- Libraries/Inverse_Kinematics/Inverse_Kinematics.h
+++ Libraries/Inverse_Kinematics/Inverse_Kinematics.h
@@ -3,4 +3,4 @@
 #include <cstdint>
 #include <string>
 
-std::vector<double> IK_solver(float x, float y, float z);
+std::vector<double> IK_solver(float x, float y, float z, float a, float b, float c);
